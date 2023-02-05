72717d9eb353028f47feb7784d49a801e191ffa5
Xin Lei Lin <xinlei55555@gmail.com>
2023-02-05 10:12:25 -0500

    fixed a few time

--- IMAGE_vision/checking-something.py
+++ IMAGE_vision/checking-something.py
@@ -1,5 +1,7 @@
 import numpy as np
 
-x=np.load("val_data/actor/iR6M4P6XqB0.npy")
+# x=np.load("IMAGE_vision/val_data/actor/iR6M4P6XqB0.npy")
+x=np.load("IMAGE_vision/val_data/yesterday/N4n5CDpyX3w.npy")
+
 print(x)
 print(x.shape)
