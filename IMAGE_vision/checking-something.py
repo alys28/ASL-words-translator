99efc9035aa18b5104ef5b38397f54b929dc53e3
freakboy123 <alyshariff2810@gmail.com>
2023-02-07 18:59:02 -0500

    training processing done

--- IMAGE_vision/checking-something.py
+++ IMAGE_vision/checking-something.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # x=np.load("IMAGE_vision/val_data/actor/iR6M4P6XqB0.npy")
-x=np.load("IMAGE_vision/val_data/yesterday/N4n5CDpyX3w.npy")
+x=np.load("train_data/all/XiAhG4dheT4.npy")
 
 print(x)
 print(x.shape)
