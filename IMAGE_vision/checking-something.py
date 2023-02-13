c85feb30bfb24434209cd18ca2fb8795a197b5ae
Aly <alyshariff2810@gmail.com>
2023-02-13 11:10:18 -0500

    testing the model

--- IMAGE_vision/checking-something.py
+++ IMAGE_vision/checking-something.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # x=np.load("IMAGE_vision/val_data/actor/iR6M4P6XqB0.npy")
+x=np.load("/Users/aly/Documents/Programming/Apps/Machine Learning/ASL Converter/IMAGE_vision/test_data/24/UlRyKsP78qQ.npy")
 x=np.load("IMAGE_vision/val_data/all/sdYIkIbvdQw.npy")
 
 print(x)
