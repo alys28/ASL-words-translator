bd3681408fd43926bf9cdc8a4307015dc9135c04
Xin Lei Lin <xinlei55555@gmail.com>
2023-02-14 00:34:29 -0500

    new changes

--- IMAGE_vision/checking-something.py
+++ IMAGE_vision/checking-something.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # x=np.load("IMAGE_vision/val_data/actor/iR6M4P6XqB0.npy")
-x=np.load("IMAGE_vision/val_data/all/sdYIkIbvdQw.npy")
+x=np.load("IMAGE_vision/val_data_restricted_labels/cheese/Nr2W7xF_jyA.npy")
 
 print(x)
 print(x.shape)
