e129389f8438dc2117f1c532bc06c70fd3d47a8e
Xin Lei Lin <xinlei55555@gmail.com>
2023-03-02 23:13:54 -0500

    stated roughtly the background remover, although weird error, mais bon

--- IMAGE_vision/checking-something.py
+++ IMAGE_vision/checking-something.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-# x=np.load("IMAGE_vision/val_data/actor/iR6M4P6XqB0.npy")
+x=np.load("IMAGE_vision/val_data/actor/iR6M4P6XqB0.npy")
 # x=np.load("/Users/aly/Documents/Programming/Apps/Machine Learning/ASL Converter/IMAGE_vision/test_data/24/UlRyKsP78qQ.npy")
-x = np.load("C:/Users/malik/Desktop/ASL-Aly/ASL-words-translator/IMAGE_vision/train_data_restricted_labels/friendly/2nXrJ_7NOgE.npy")
+# x = np.load("C:/Users/malik/Desktop/ASL-Aly/ASL-words-translator/IMAGE_vision/train_data_restricted_labels/friendly/2nXrJ_7NOgE.npy")
 print(x)
 print(x.shape)
